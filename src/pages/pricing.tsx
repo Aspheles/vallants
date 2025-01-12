9f7d819f61b8919303242611549f41d42db9dc8c
Asphele <yavuzkaand@hotmail.com>
2025-01-12 23:49:48 +0100

    updated pricing page

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,213 +1,151 @@
 import React from "react";
-import { FaQuestionCircle } from "react-icons/fa";
+import { useModal } from "../utils/ModalContext";
 
 type Plan = {
   name: string;
-  price: string;
-  features: {
-    [key: string]: boolean;
-  };
-};
-
-type Feature = {
-  name: string;
+  description: string;
+  originalPrice: number;
+  discount?: number;
+  features: string[];
 };
 
 const Pricing: React.FC = () => {
+  const { openModal } = useModal();
+
   const plans: Plan[] = [
     {
-      name: "Basic Plan",
-      price: "€50",
-      features: {
-        domainHosting: false,
-        technicalSupport: true,
-        storage: false,
-        customDomain: false,
-      },
+      name: "Starter Plan",
+      description:
+        "Ideal for small businesses or individuals looking to establish an online presence with essential features.",
+      originalPrice: 500,
+      features: [
+        "Up to 5 custom-designed pages",
+        "Mobile and tablet responsive design",
+        "Integration with social media (links, feeds)",
+        "SSL certificate setup for site security",
+        "Domain configuration",
+      ],
+    },
+    {
+      name: "Professional Plan",
+      description:
+        "Perfect for growing businesses that need additional features to engage visitors and optimize performance.",
+      originalPrice: 700,
+      discount: 10,
+      features: [
+        "Up to 7 custom-designed pages",
+        "Mobile and tablet responsive design",
+        "Integration with social media (links, feeds)",
+        "Basic SEO setup (meta tags, alt text, keyword optimization)",
+        "SSL certificate setup for site security",
+        "Contact form with email notifications",
+        "Google Analytics integration for traffic insights",
+      ],
     },
     {
       name: "Premium Plan",
-      price: "€150",
-      features: {
-        domainHosting: true,
-        technicalSupport: true,
-        storage: true,
-        customDomain: true,
-      },
+      description:
+        "A comprehensive package tailored for businesses seeking advanced features and enhanced functionality.",
+      originalPrice: 1000,
+      features: [
+        "Up to 10 custom-designed pages",
+        "Mobile and tablet responsive design",
+        "Advanced SEO setup (meta tags, alt text, keyword optimization, XML sitemap)",
+        "Integration with social media (links, feeds)",
+        "Contact form with email notifications",
+        "SSL certificate setup for site security",
+        "Domain configuration",
+        "Google Analytics integration for traffic insights",
+        "Custom email account setup",
+      ],
     },
   ];
 
-  const featuresList: Feature[] = [
-    { name: "Domain Hosting" },
-    { name: "Technical Support" },
-    { name: "Storage" },
-    { name: "Custom Domain" },
-  ];
+  const calculateDiscountedPrice = (price: number, discount?: number) => {
+    if (discount) {
+      return price - price * (discount / 100);
+    }
+    return price;
+  };
 
   return (
-    <>
-      <div className="h-[45rem] bg-gradient-to-b from-[#1D0D4C] to-[#6A00A1] text-white px-4 w-full mx-auto">
-        <h1 className="text-4xl font-bold text-white py-16 text-center">
+    <div className="bg-gradient-to-b from-[#1D0D4C] to-[#6A00A1] text-white px-8 py-16 w-full mx-auto">
+      <div className="max-w-screen-md mx-auto mb-12 text-center">
+        <h2 className="mb-4 text-4xl font-extrabold tracking-tight text-white">
           Plans & Pricing
-        </h1>
-
-        <div className="flex justify-center">
-          <div className="max-w-4xl w-full text-[#1D0D4C]">
-            <div className="flex items-center justify-center bg-gradient-to-b from-[#420674] to-[#1D0D4C] rounded-t-lg text-white w-[22rem] h-[6rem] mx-4 shadow-lg">
-              <div className="flex flex-row items-center justify-center space-x-4">
-                <h2 className="text-2xl font-semibold">Basic Package</h2>
-
-                {/* Divider using React Icon */}
-                <div className="border-l-2 border-white h-[3rem] mx-4"></div>
-
-                <p className="text-5xl font-semibold">€500</p>
-              </div>
-            </div>
-            <div className="rounded-md shadow-lg border">
-              {/* Features List */}
-              <ul className="">
-                <li className="flex items-center h-[3rem]  bg-white px-4 ">
-                  <p>
-                    Up to{" "}
-                    <span className="font-semibold">
-                      5 custom-designed pages (Home, About, Services, Contact,
-                      Blog)
+        </h2>
+        <p className="mb-5 text-lg font-light text-gray-200 sm:text-xl">
+          Here at Vallants, we focus on affordable solutions for starting
+          businesses.
+        </p>
+      </div>
+      <div className="grid lg:grid-cols-3 sm:gap-8 gap-8">
+        {plans.map((plan) => (
+          <div
+            key={plan.name}
+            className="relative flex flex-col max-w-xl p-8 mx-auto text-center text-gray-900 bg-white border border-gray-200 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+          >
+            <h3 className="mb-4 text-3xl font-bold text-[#1D0D4C]">
+              {plan.name}
+            </h3>
+            <p className="mb-6 text-base font-light text-gray-700 sm:text-lg">
+              {plan.description}
+            </p>
+            <div className="flex flex-col items-center mb-6 relative">
+              {plan.discount ? (
+                <>
+                  <span className="relative text-5xl text-white font-extrabold bg-[#6A00A1] py-4 px-10 rounded-full shadow-md">
+                    <span className="absolute top-0 left-0 bg-red-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-md">
+                      -{plan.discount}%
                     </span>
-                  </p>
-                </li>
-                <li className="flex items-center h-[3rem] bg-[#F4F2FA] px-4">
-                  <p>
-                    Mobile and tablet{" "}
-                    <span className="font-semibold">responsive design</span>
-                  </p>
-                </li>
-                <li className="flex items-center h-[3rem] bg-white px-4">
-                  <p>
-                    Basic <span className="font-semibold">SEO</span> setup (meta
-                    tags, alt text, keyword optimization)
-                  </p>
-                </li>
-                <li className="flex items-center h-[3rem] bg-[#F4F2FA] px-4">
-                  <p>
-                    Integration with{" "}
-                    <span className="font-semibold">social media</span> (links,
-                    feeds)
-                  </p>
-                </li>
-                <li className="flex items-center h-[3rem] bg-white px-4">
-                  <p>
-                    Contact form with{" "}
-                    <span className="font-semibold">email notifications</span>
-                  </p>
-                </li>
-                <li className="flex items-center h-[3rem] bg-[#F4F2FA] px-4">
-                  <p>
-                    SSL certificate setup for{" "}
-                    <span className="font-semibold">site security</span>
-                  </p>
-                </li>
-                <li className="flex items-center h-[3rem] bg-white px-4">
-                  <p>Domain configuration</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+                    €
+                    {calculateDiscountedPrice(
+                      plan.originalPrice,
+                      plan.discount
+                    ).toFixed(2)}
+                  </span>
 
-      <div className="flex justify-center py-12 px-4">
-        <div className="max-w-4xl w-full text-[#1D0D4C]">
-          {/* Addons Title and Plans */}
-          <div className="flex flex-wrap items-center justify-between mb-6 gap-4">
-            {/* Addons Title */}
-            <div className="flex items-center space-x-4">
-              <p className="text-2xl hover:text-3xl transition-all cursor-pointer">
-                <FaQuestionCircle />
-              </p>
-              <p className="text-4xl font-bold drop-shadow-xl text-[#6A00A1]">
-                Addons
-              </p>
+                  <span className="text-base text-gray-500 line-through mt-1">
+                    €{plan.originalPrice.toFixed(2)}
+                  </span>
+                </>
+              ) : (
+                <span className="text-5xl text-white font-extrabold bg-[#6A00A1] py-4 px-10 rounded-full shadow-md">
+                  €{plan.originalPrice.toFixed(2)}
+                </span>
+              )}
             </div>
-
-            {/* Plans Section */}
-            <div className="flex flex-wrap justify-end gap-2">
-              {plans.map((plan) => (
-                <div
-                  key={plan.name}
-                  className="flex items-center justify-center bg-gradient-to-b from-[#420674] to-[#1D0D4C] rounded-lg text-white w-full sm:w-[14rem] h-[6rem] shadow-lg"
-                >
-                  <div className="flex flex-col items-center justify-center text-center">
-                    <h2 className="text-xl sm:text-2xl font-semibold">
-                      {plan.name}
-                    </h2>
-                    <p className="text-3xl sm:text-4xl font-semibold">
-                      {plan.price}
-                    </p>
-                  </div>
-                </div>
+            <ul role="list" className="mb-8 space-y-4 text-left flex-grow">
+              {plan.features.map((feature, index) => (
+                <li key={index} className="flex items-center space-x-3">
+                  <svg
+                    className="flex-shrink-0 w-6 h-6 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="text-gray-800">{feature}</span>
+                </li>
               ))}
+            </ul>
+            <div className="mt-auto">
+              <a
+                onClick={openModal}
+                className="text-white bg-[#6A00A1] hover:bg-purple-700 font-medium rounded-full text-lg px-6 py-3 transition-all duration-300 cursor-pointer"
+              >
+                Contact Us
+              </a>
             </div>
           </div>
-
-          {/* Features Table */}
-          <div className="rounded-md shadow-lg border overflow-x-auto">
-            <table className="min-w-full table-auto rounded-md overflow-hidden">
-              <thead className="bg-[#420674] text-white">
-                <tr>
-                  <th className="px-4 py-4 text-left text-sm sm:text-base font-semibold">
-                    Features
-                  </th>
-                  {plans.map((plan) => (
-                    <th
-                      key={plan.name}
-                      className="px-4 sm:px-8 py-4 text-center text-sm sm:text-base font-semibold"
-                    >
-                      {plan.name}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {featuresList.map((feature, rowIndex) => {
-                  // Normalize the feature name
-                  const featureKey = feature.name
-                    .toLowerCase()
-                    .replace(/ /g, "")
-                    .replace(/[^a-z]/g, ""); // Remove special characters
-
-                  return (
-                    <tr
-                      key={feature.name}
-                      className={`${
-                        rowIndex % 2 === 0 ? "bg-white" : "bg-[#F4F2FA]"
-                      }`}
-                    >
-                      <td className="px-4 sm:px-8 py-4 text-left text-sm sm:text-base font-semibold">
-                        {feature.name}
-                      </td>
-                      {plans.map((plan) => (
-                        <td
-                          key={plan.name}
-                          className="px-4 sm:px-8 py-4 text-center"
-                        >
-                          <span
-                            className={`w-4 h-4 rounded-full inline-block ${
-                              plan.features[featureKey]
-                                ? "bg-green-500"
-                                : "bg-gray-300"
-                            }`}
-                          ></span>
-                        </td>
-                      ))}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
