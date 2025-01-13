133d937a0cbb950f079475d796e681e48c69acbc
Asphele <yavuzkaand@hotmail.com>
2025-01-13 20:53:36 +0100

    added analytics package

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { ModalProvider } from "./utils/ModalContext.tsx";
 import ContactFormModal from "./components/ContactForm.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
       <ModalProvider>
         <ContactFormModal />
