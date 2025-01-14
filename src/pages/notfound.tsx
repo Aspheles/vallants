4ccfd83c7fe5425a9a4b892aa9807dc3333edbb9
Asphele <yavuzkaand@hotmail.com>
2025-01-14 05:00:10 +0100

    added styling to not found page

--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound: React.FC = () => {
-  return <h1>Page not found!</h1>;
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-white text-gray-800 px-4">
+      <img
+        className="w-[10rem] mb-6"
+        src="/Vallants_logo.png"
+        alt="Vallants Logo"
+      />
+      <h1 className="text-4xl font-bold text-center text-gray-900 mb-4">
+        Something went wrong
+      </h1>
+      <p className="text-xl text-center text-gray-600 mb-8">
+        We're sorry, but the page you're looking for doesn't exist or has been
+        moved.
+      </p>
+      <Link to={"/"}>
+        <button className="px-6 py-3 bg-[#6A00A1] text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition duration-200">
+          Go Back
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
